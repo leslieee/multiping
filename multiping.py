a384c624782a0b08a34eec50957e471757bde939
leslie <hushuangsoutheastasia@gmail.com>
2017-09-02 01:10:09 +0800

    Add files via upload

---
+++ multiping.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+#encoding=utf8  
+#leslie 2017.9.1
+from threading import Thread
+import time
+import os
+import commands
+import urllib
+import json
+
+# 此处加入ip 注意列表格式-_-
+ip = [  '101.254.176.225',
+        '222.186.56.1',
+        '103.236.136.1',
+        '103.42.212.162', 
+        '47.89.38.47', 
+        '103.95.29.1',
+        '103.88.45.1', 
+        '103.86.46.1',
+        '92.223.73.113',
+        '103.19.8.83',
+        '92.223.80.1',
+        '45.76.70.196', 
+        '103.82.5.171', 
+        '185.199.225.67',
+        '216.189.158.66',
+        '109.230.219.1',
+        '185.194.236.25',
+        '89.34.26.50',
+        '176.31.141.12',
+    ]
+
+region = []
+delay = []
+lost = []
+sent = []
+
+def requestInfo(ip,i):
+    response = urllib.urlopen('https://ip.huomao.com/ip?ip=' + ip)
+    jsonstr = json.loads(response.read())
+    region[i] = jsonstr["country"] + jsonstr["province"] + jsonstr["city"] + jsonstr["isp"]
+
+def printDelay():
+    while 1:
+        os.system('clear')
+        for i in range(len(ip)):
+            print ip[i] + "\t" + "dalay:" + delay[i] + " " + "lost:" + str(lost[i]) + " \t" + "sent:" + str(sent[i]) + "\t" + region[i]
+        time.sleep(1)
+
+def ping(ip,i):
+    while 1:
+        output = commands.getoutput("ping -c 1 -t 1 " + ip + "|grep time|awk -F '=' '{print $4}'")
+        if output == "":
+            lost[i] += 1
+            output = "          "
+        else:
+            if len(output) < 10:
+                output += " "
+        sent[i] += 1
+        delay[i] = output
+        time.sleep(0.5)
+
+def main():
+    # init
+    for i in range(len(ip)):
+        region.append("")
+        delay.append("")
+        lost.append(0)
+        sent.append(0)
+    for i in range(len(ip)):
+        t = Thread(target=ping, args=(ip[i],i))
+        t.start()
+    for i in range(len(ip)):
+        t = Thread(target=requestInfo, args=(ip[i],i))
+        t.start()
+    th = Thread(target=printDelay)
+    th.start()
+    q = raw_input()
+    if q == "q":
+        sys.exit(0)
+
+if __name__ == '__main__':
+	main()
