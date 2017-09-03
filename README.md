a74cd7c9bba67b977134c6c86fd5a1578521b1f2
leslie <leslie@leslie.lan>
2017-09-04 02:34:28 +0800

    引入三方pure python ping, 实现跨平台运行

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ Network packet loss analysis script
 
 同时ping多个服务器并统计丢包率, 观察线路的国际出口情况, 便于ss/ssr线路选择
 # 效果图
-![](multiping.png)
-# 运行
+![](show.png)
+# 运营环境
+> pyhton2.7.*
+# 支持平台
+Mac, Linux, Windows
+# 启动
 > python multiping.py
+
+(mac需要加sudo运行)
+# 停止
+ctrl + z, 或者直接关掉terminal
 # 加入新的ip
 在multiping.py的ip列表中加入, 注意格式 'your ip',
 >ip = [  '101.254.176.225', '222.186.56.1', '103.236.136.1', ...
--- library.py
+++ library.py
@@ -1,37 +1,5 @@
-#!/usr/bin/python
-#encoding=utf8  
-# leslie
+#!/usr/bin/env python
 
-# 此处加入ip 注意列表格式-_-
-ip = [  '101.254.176.225',
-        '222.186.56.1',
-        '103.236.136.1',
-        '103.42.212.162', 
-        '47.89.38.47', 
-        '103.95.29.1',
-        '103.88.45.1', 
-        '103.86.46.1',
-        '92.223.73.113',
-        '103.19.8.83',
-        '92.223.80.1',
-        '45.76.70.196', 
-        '103.82.5.171', 
-        '216.45.55.35',
-        '216.189.158.66',
-        '185.199.225.67',
-        '66.70.158.60',
-        '109.230.219.1',
-        '185.194.236.25',
-        '89.34.26.50',
-        '109.235.69.33',
-        '176.31.141.12',
-        '5.196.116.230',
-    ]
-
-# 第三方pure pyhton ping 实现
-# 修改了my_ID变量的获取方式
-# 修改了方法返回类型
-##########################################################
 """
     A pure python ping implementation using raw socket.
 
@@ -101,7 +69,7 @@ ip = [  '101.254.176.225',
 """
 
 
-import os, sys, socket, struct, select, time, random
+import os, sys, socket, struct, select, time
 
 if sys.platform == "win32":
     # On Windows, the best timer is time.clock()
@@ -218,9 +186,7 @@ def do_one(dest_addr, timeout):
             raise socket.error(msg)
         raise # raise the original error
 
-    # my_ID = os.getpid() & 0xFFFF
-    # my_ID = threading.current_thread().ident & 0xFFFF
-    my_ID = int(str(random.random()).replace("0.", "")) & 0xFFFF
+    my_ID = os.getpid() & 0xFFFF
 
     send_one_ping(my_socket, dest_addr, my_ID)
     delay = receive_one_ping(my_socket, my_ID, timeout)
@@ -239,137 +205,17 @@ def verbose_ping(dest_addr, timeout = 1, count = 1):
         try:
             delay  =  do_one(dest_addr, timeout)
         except socket.gaierror, e:
-            #print "failed. (socket error: '%s')" % e[1]
-            return ""
+            print "failed. (socket error: '%s')" % e[1]
             break
 
         if delay  ==  None:
-            #print "failed. (timeout within %ssec.)" % timeout
-            return ""
+            print "failed. (timeout within %ssec.)" % timeout
         else:
             delay  =  delay * 1000
             # print "get ping in %0.2fms" % delay
-            return "%0.3f ms" % delay
+            return "%0.2fms" % delay
     print
 
 
-
-
-
-# multiping 实现
-# leslie
-################################################
-
-from threading import Thread
-import time
-import os
-import commands
-import urllib
-import json
-import platform
-import sys
-# import library
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-region = []
-delay = []
-lost = []
-loststr = []
-sent = []
-sentstr = []
-sysstr = platform.system()
-
-def requestInfo(ip,i):
-    response = urllib.urlopen('https://ip.huomao.com/ip?ip=' + ip)
-    jsonstr = json.loads(response.read())
-    region[i] = jsonstr["country"] + jsonstr["province"] + jsonstr["city"] + jsonstr["isp"]
-
-def printDelay():
-    while 1:
-        if sysstr == "Darwin":
-            os.system('clear')
-            for i in range(len(ip)):
-                print ip[i] + "\t" + "dalay:" + delay[i] + " " + "lost:" + loststr[i] + "" + "sent:" + str(sent[i]) + "\t" + region[i]
-        elif sysstr == "Linux":
-            os.system('clear')
-            for i in range(len(ip)):
-                print ip[i] + "\t" + "dalay:" + delay[i] + " " + "lost:" + loststr[i] + "" + "sent:" + str(sent[i]) + "\t" + region[i]
-        elif sysstr == "Windows":
-            os.system('cls')
-            for i in range(len(ip)):
-                print ip[i] + "\t" + "dalay:" + delay[i] + " " + "lost:" + loststr[i] + "" + "sent:" + sentstr[i] + "" + region[i]
-        
-        time.sleep(1)
-
-def ping(ip,i):
-    while 1:
-        # output = ""
-        # if sysstr == "Darwin":
-        #     # output = commands.getoutput("ping -c 1 -t 1 " + ip + "|grep time|awk -F '=' '{print $4}'")
-        #     output = verbose_ping(ip)
-        # elif sysstr == "Linux":
-        #     output = commands.getoutput("ping -c 1 -w 1 " + ip + "|grep time|awk -F '=' '{print $4}'")
-        # elif sysstr == "Windows":
-        #     output = commands.getoutput("ping -n 1 -w 500 " + ip).replace("'{ '", "").decode("GBK").encode("UTF-8")
-        #     output = commands.getoutput("echo " + output + "|grep 时间=").replace("'{ '", "").decode("GBK").encode("UTF-8")
-        #     output = commands.getoutput("echo " + output + "|awk -F '=' '{print $3}'").replace("'{ '", "").decode("GBK").encode("UTF-8")
-        #     # output = commands.getoutput("ping -n 1 -w 500 " + ip + "|grep 时间=|awk -F '=' '{print $3}'")
-
-        output = verbose_ping(ip)
-
-        if output == "":
-            output = "          "
-            lost[i] += 1
-            le = len(str(lost[i]))
-            if le == 1:
-                loststr[i] = str(lost[i]) + "  "
-            elif le == 2:
-                loststr[i] = str(lost[i]) + " "
-        else:
-            if len(output) < 10:
-                output += " "
-        sent[i] += 1
-        le = len(str(sent[i]))
-        if le == 1:
-            sentstr[i] = str(sent[i]) + "    "
-        elif le == 2:
-            sentstr[i] = str(sent[i]) + "   "
-        elif le == 3:
-            sentstr[i] = str(sent[i]) + "  "
-        elif le == 4:
-            sentstr[i] = str(sent[i]) + " "
-        delay[i] = output
-        time.sleep(0.5)
-
-def main():
-    # init
-    for i in range(len(ip)):
-        region.append("")
-        delay.append("")
-        lost.append(0)
-        loststr.append("   ")
-        sent.append(0)
-        sentstr.append("     ")
-
-    for i in range(len(ip)):
-        t = Thread(target=ping, args=(ip[i],i))
-        t.start()
-    for i in range(len(ip)):
-        t = Thread(target=requestInfo, args=(ip[i],i))
-        t.start()
-    th = Thread(target=printDelay)
-    th.start()
-    q = raw_input()
-    if q == "q":
-        sys.exit(0)
-
 if __name__ == '__main__':
-	main()
-
-
-
-
-
-
-
+    print verbose_ping("speedss.ml")
